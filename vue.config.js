bd42e4afba9429ccf0b26b8a51a5593da19d059a
shpfive <shpfive@googlemail.com>
2020-05-16 22:17:56 +0200

    cleanup

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-// vue.config.js
-const autoprefixer = require("autoprefixer");
-const plugin = autoprefixer({ grid: true });
-
 module.exports = {
   css: {
     loaderOptions: {
@@ -9,16 +5,5 @@ module.exports = {
         prependData: `@import "@/scss/_variables.scss";`
       }
     }
-  },
-  configureWebpack: config => {
-    // Define autoprefixer for vue files
-    let index = config.module.rules.findIndex(item => item.test.test("*.vue"));
-    let rule = config.module.rules[index];
-    rule.use[0].options = {
-      ...rule.use[0].options,
-      postcss: {
-        plugins: [plugin]
-      }
-    };
   }
 };
