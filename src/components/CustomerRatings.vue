7937e8091de4c5c994d3fb6dc5acb13c7a057ffa
shpfive <shpfive@googlemail.com>
2020-05-16 23:45:48 +0200

    cleanup

--- src/components/CustomerRatings.vue
+++ src/components/CustomerRatings.vue
@@ -1,53 +1,13 @@
 <template>
   <div class="customerRatings text--default">
     <div class="ratings">
-      <span class="star fa-stack">
+      <span class="star fa-stack" v-for="i in [1, 3, 5, 7, 9]" :key="i">
         <i
           class="far fa-star color--grey-200"
           :class="{
-            'color--yellow': stars >= 1,
-            'fas fa-star-half-alt': stars === 1,
-            fa: stars > 1
-          }"
-        ></i>
-      </span>
-      <span class="star fa-stack">
-        <i
-          class="far fa-star color--grey-200"
-          :class="{
-            'color--yellow': stars >= 3,
-            'fas fa-star-half-alt': stars === 3,
-            fa: stars > 3
-          }"
-        ></i>
-      </span>
-      <span class="star fa-stack">
-        <i
-          class="far fa-star color--grey-200"
-          :class="{
-            'color--yellow': stars >= 5,
-            'fas fa-star-half-alt': stars === 5,
-            fa: stars > 5
-          }"
-        ></i>
-      </span>
-      <span class="star fa-stack">
-        <i
-          class="far fa-star color--grey-200"
-          :class="{
-            'color--yellow': stars >= 7,
-            'fas fa-star-half-alt': stars === 7,
-            fa: stars > 7
-          }"
-        ></i>
-      </span>
-      <span class="star fa-stack">
-        <i
-          class="far fa-star color--grey-200"
-          :class="{
-            'color--yellow': stars >= 9,
-            'fas fa-star-half-alt': stars === 9,
-            fa: stars > 9
+            'color--yellow': stars >= i,
+            'fas fa-star-half-alt': stars === i,
+            fa: stars > i
           }"
         ></i>
       </span>
